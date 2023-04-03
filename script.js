f4917fad384f9c38a602ea5ae90887d9bcde7fc5
Joshua-Eggleston <jeggleston1980@gmail.com>
2023-04-03 13:42:45 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const quizData =[
+    {
+        Q1: "What is the purpose of the <track> tag, and when should it be used?",
+        a: 'The <track> tag is used for specifying subtitles. It is typically applied as a child of the <audio> and <video> tags.',
+        b: 'The <track> tag is used for specifying subtitles. It is typically applied as a child of the <video> tag.',
+        c: 'The <track> tag is used for specifying subtitles, captions, and other types of time-based text. It is typically applied as a child of the <video> tag.',
+        d: 'The <track> tag is used for specifying subtitles, captions, and other types of time-based text. It is typically applied as a child of the <audio> and <video> tag.',
+        correct: 'd'
+    },
+
+]

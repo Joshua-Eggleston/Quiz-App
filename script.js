1b8ca17f8cb417273f3a5fc5369ddc4323a9d8a0
joshua-eggleston <jeggleston1980@gmail.com>
2023-05-06 15:25:35 -0500

    fixed media queriie for hi and body tag issue

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const quizData =[
     {
-        question: `Q1. What is the purpose of the <plaintext> <track> tag, and when should it be used?`,
+        question: `Q1. What is the purpose of the <plaintext><track>  tag, and when should it be used?`,
         a: 'The <track> tag is used for specifying subtitles. It is typically applied as a child of the <audio> and <video> tags.',
         b: 'The <track> tag is used for specifying subtitles. It is typically applied as a child of the <video> tag.',
         c: 'The <track> tag is used for specifying subtitles, captions, and other types of time-based text. It is typically applied as a child of the <video> tag.',
